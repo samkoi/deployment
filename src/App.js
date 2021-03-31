792ab5b89e4f550f02b696a862a10f6009994f36
sam <playstore.samit@gmail.com>
2021-03-31 23:34:08 +0200

    Frontend with test data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import test_data from "./test_data.json";
+import DeploymentPlans from "./DeploymentPlans";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TableContainer component={Paper}>
+        <Table size="small">
+          <TableHead>
+            <TableRow className={"primaryRow"}>
+              <TableCell/>
+              <TableCell className={"fieldName"}>Deployment ID</TableCell>
+              <TableCell className={"fieldName"}>Deployment Name</TableCell>
+              <TableCell className={"fieldName"}>Creator</TableCell>
+              <TableCell className={"fieldName"}>Created</TableCell>
+              <TableCell className={"fieldName"}>Locked</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {test_data.deploymentPlans.map(deploymentPlans => {
+              return <DeploymentPlans key = {deploymentPlans.id} {...deploymentPlans} />;
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
